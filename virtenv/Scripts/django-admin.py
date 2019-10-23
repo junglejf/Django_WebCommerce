a1eb1f0101c25e82ea6352d7146a559629979867
junglejf <jfcchagas@id.uff.br>
2019-10-23 00:33:29 -0300

    starting

---
+++ virtenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jungl\onedrive\documentos\django_dweb\virtenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
